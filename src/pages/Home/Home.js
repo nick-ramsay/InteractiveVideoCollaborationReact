6b0bf0a22e3c1f0712cb8926dc7b3379f66b8047
Nick Ramsay <NickRamsay1989@gmail.com>
2020-01-15 19:39:45 +1100

    Added initial canvas code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,9 +5,34 @@ import "./style.css";
 
 class Home extends Component {
     state = {
-
+        currentVideoTime: 0.00
     }
 
+    componentDidMount() {
+        this.checkVideoTime();
+        this.renderCanvas();
+    }
+
+    currentVideoTime = () => {
+        var testVideo = document.getElementById("testVideo");
+        this.setState({ currentVideoTime: testVideo.currentTime })
+    }
+
+    checkVideoTime = () => setInterval(() => {
+        this.currentVideoTime()
+        this.renderCanvas();
+    }, 100);
+
+    renderCanvas = () => {
+        var c = document.getElementById("cv1");
+        
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0, 0, c.width, c.height); 
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText(this.state.currentVideoTime, 10, 125);
+             
+    }
 
     render() {
         return (
@@ -15,9 +40,13 @@ class Home extends Component {
                 <Navbar />
                 <div className="container pt-4">
                     <div className="col-md-12 my-5 text-center">
-                        <video controls>
-                            <source src={Video} type="video/mp4" />
-                        </video>
+                        <div>
+                            <video id="testVideo" controls>
+                                <source src={Video} type="video/mp4" />
+                            </video>
+                            <canvas class="canvas" id="cv1">{this.state.currentVideoTime}</canvas>
+                        </div>
+                        <h3><strong>Seconds Elapsed: {this.state.currentVideoTime}</strong></h3>
                     </div>
                 </div>
             </div>

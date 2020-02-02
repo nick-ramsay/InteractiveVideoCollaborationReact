df991e979e266bc615c7279b3ee4b48ee41e5c0f
Nick Ramsay <NickRamsay1989@gmail.com>
2020-02-02 12:58:32 +1100

    Committing most recent canvas progress, original tab

--- src/pages/OriginalDemo/OriginalDemo.js
+++ src/pages/OriginalDemo/OriginalDemo.js
@@ -1,36 +1,104 @@
 import React, { Component } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Video from "../../videos/2DOFSpringMassSystemProof.mp4";
+import MathFunctions from "../../simulations/MathFunctions";
+import PhysicsSimulation from "../../simulations/PhysicsSimulation";
+import mediaSource from "../../videos/2DOFSpringMassSystemProof.mp4";
 import "./style.css";
 
 var parentStyle = {
-    margin: "0 auto", 
+    margin: "0 auto",
     width: "888px"
 }
 
+var v;
+var c;
+var ctx;
+
 class OriginalDemo extends Component {
 
+    initializeCanvas = () => {
+        v = document.getElementById("myVideo")
+        c = document.getElementById("myCanvas")
+        ctx = c.getContext("2d")
+    }
+
     state = {
+        videoPlaying: false,
+        videoMuted: false
     }
 
     componentDidMount() {
+        this.initializeCanvas();
+        this.refreshCanvasVideo();
+    }
+
+    renderVideo = () => {
+        ctx.drawImage(v, 0, 0);
+    }
+
+    refreshCanvasVideo = () => setInterval(() => {
+        this.renderVideo();
+    }, 20);
+
+    playVideo = event => {
+        event.preventDefault();
+
+        this.setState({ videoPlaying: true });
+    }
+
+    pauseVideo = event => {
+        event.preventDefault();
+
+        this.setState({ videoPlaying: false }).then(this.renderVideo);
+    }
+
+    muteVideo = event => {
+        event.preventDefault();
+
+        this.setState({ videoMuted: true });
+    }
+
+
+    unmuteVideo = event => {
+        event.preventDefault();
+
+        this.setState({ videoMuted: false });
     }
 
     render() {
         return (
-            <div>
+            <div className="originalDemoContent">
                 <Navbar />
                 <div className="container pt-4">
-                    <div className="col-md-12 my-5 text-center">
+                    <div className="col-md-12 my-5 text-center canvasContainer">
                         <h1><strong>React Conversion of Original Demo</strong></h1>
-                        <div className="row canvasContainer">
+                        <div className="row d-none">
                             <div class="parent" style={parentStyle}>
-                                <canvas id="myCanvas" width="888" height="500" ></canvas>
-                                <div id="custom-seekbar">
-                                    <span></span>
-                                </div>
-                                <div class="mute"></div>
-                                <div id="myButton" class='button play' type="button"></div>
+                                <video id="myVideo" src={mediaSource} controls></video>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div class="parent" style={parentStyle}>
+                                <canvas id="myCanvas" width="888" height="500"></canvas>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-3 mb-1">
+                                {!this.state.videoPlaying &&
+                                    <button className="btn-sm btn-success" name="playBtn" onClick={this.playVideo}>Play</button>
+                                }
+                                {this.state.videoPlaying &&
+                                    <button className="btn-sm btn-danger" name="pauseBtn" onClick={this.pauseVideo}>Pause</button>
+                                }
+                            </div>
+                            <div className="col-md-3 mb-1">
+                                {!this.state.videoMuted &&
+                                    <button className="btn-sm btn-warning" name="muteBtn" onClick={this.muteVideo}>Mute</button>
+                                }
+                                {this.state.videoMuted &&
+                                    <button className="btn-sm btn-primary" name="unmutedBtn" onClick={this.unmuteVideo}>Unmute</button>
+                                }
+
                             </div>
                         </div>
                     </div>
